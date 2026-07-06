0ca3dc2f294f982e535b9a31aac853a90a0738dc
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2026-07-06 04:06:12 +0200

    replace indexing types with generic type variant

---
+++ Il2CppInspector.Common/Next/Indexing/Il2CppMetadataIndex.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using VersionedSerialization;
+
+namespace Il2CppInspector.Next.Indexing;
+
+file static class Il2CppMetadataIndex
+{
+    public const int Invalid = -1;
+    public const char NumberZeroChar = '0';
+}
+
+public struct Il2CppMetadataIndex<T>(int value) : IReadable, IEquatable<Il2CppMetadataIndex<T>>
+{
+    public int Value { get; private set; } = value;
+
+    public static readonly string TagPrefix = $"{typeof(T).Name}";
+
+    public static readonly string SizeTag = $"{typeof(T).Name}Size";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int GetSizeFromTag(in StructVersion version)
+    {
+        // Fallback to the default for unsupported configurations.
+        if (version < MetadataVersions.V380 || version.Tag == null)
+        {
+            return sizeof(int);
+        }
+
+        // Get the position of the size tag in the version tag.
+        // Bail out if not found.
+        var sizeTagPosition = version.Tag.IndexOf(SizeTag, StringComparison.Ordinal);
+        if (sizeTagPosition == -1)
+        {
+            return sizeof(int);
+        }
+
+        // Get the number that follows immediately after the size tag, and convert it to the actual size.
+        var numberChar = version.Tag[sizeTagPosition + SizeTag.Length];
+        return numberChar - Il2CppMetadataIndex.NumberZeroChar;
+    }
+
+    public static string CreateTagBySize(int size)
+    {
+        Debug.Assert(size is sizeof(byte) or sizeof(ushort) or sizeof(uint));
+        return $"{SizeTag}{size}";
+    }
+
+    public static int GetSizeByCount(int count) => count switch
+    {
+        < byte.MaxValue => sizeof(byte),
+        < ushort.MaxValue => sizeof(ushort),
+        _ => sizeof(uint)
+    };
+
+    public static string CreateTagByCount(int count)
+        => CreateTagBySize(GetSizeByCount(count));
+
+    void IReadable.Read<TReader>(ref Reader<TReader> reader, in StructVersion version)
+    {
+        var size = GetSizeFromTag(in version);
+
+        if (size == sizeof(int))
+        {
+            Value = reader.ReadPrimitive<int>();
+        }
+        else if (size == sizeof(ushort))
+        {
+            var value = reader.ReadPrimitive<ushort>();
+            Value = value == ushort.MaxValue ? Il2CppMetadataIndex.Invalid : value;
+        }
+        else
+        {
+            Debug.Assert(size == sizeof(byte));
+
+            var value = reader.ReadPrimitive<byte>();
+            Value = value == byte.MaxValue ? Il2CppMetadataIndex.Invalid : value;
+        }
+    }
+
+    static int IReadable.Size(in StructVersion version, in ReaderConfig config)
+        => GetSizeFromTag(in version);
+
+    #region Operators + ToString
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Il2CppMetadataIndex<T>(int value) => new(value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator int(Il2CppMetadataIndex<T> value) => value.Value;
+
+    public static bool operator ==(Il2CppMetadataIndex<T> left, Il2CppMetadataIndex<T> right)
+        => left.Value == right.Value;
+
+    public static bool operator !=(Il2CppMetadataIndex<T> left, Il2CppMetadataIndex<T> right)
+        => !(left == right);
+
+    public readonly override bool Equals(object? obj)
+        => obj is Il2CppMetadataIndex<T> other && Equals(other);
+
+    public readonly bool Equals(Il2CppMetadataIndex<T> other)
+        => this == other;
+
+    public readonly override int GetHashCode()
+        => HashCode.Combine(Value);
+
+    public readonly override string ToString() => Value.ToString();
+
+    #endregion
+}
---
+++ Il2CppInspector.Common/Next/Indexing/IndexAliases.cs
@@ -0,0 +1,66 @@
+﻿// This order matches SerializedIndexSizes from libil2cpp
+global using TypeIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.Type>;
+global using GenericParameterIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.GenericParameter>;
+global using GenericContainerIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.GenericContainer>;
+global using TypeDefinitionIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.TypeDefinition>;
+global using ParameterIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.Parameter>;
+global using InterfacesIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.Interfaces>;
+global using EventIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.Event>;
+global using PropertyIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.Property>;
+global using NestedTypeIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.NestedType>;
+global using FieldIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.Field>;
+global using DefaultValueDataIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.DefaultValueData>;
+global using MethodIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.Method>;
+
+// v108
+global using InvokerTableIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.InvokerTable>;
+global using MethodPointerTableIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.MethodPointerTable>;
+global using AdjustorThunkIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.AdjustorThunk>;
+global using GenericInstIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.GenericInst>;
+global using GenericMethodIndex = Il2CppInspector.Next.Indexing.Il2CppMetadataIndex<Il2CppInspector.Next.Indexing.GenericMethod>;
+
+global using DefaultValueIndex = int;
+global using CustomAttributeIndex = int;
+global using GenericParameterConstraintIndex = short; // sneaky
+global using VTableIndex = int;
+global using RGCTXIndex = int;
+global using StringIndex = int;
+global using StringLiteralIndex = int;
+global using ImageIndex = int;
+global using AssemblyIndex = int;
+global using InteropDataIndex = int;
+global using TypeFieldIndex = int;
+global using TypeMethodIndex = int;
+global using MethodParameterIndex = int;
+global using TypePropertyIndex = int;
+global using TypeEventIndex = int;
+global using TypeInterfaceIndex = int;
+global using TypeNestedTypeIndex = int;
+global using TypeInterfaceOffsetIndex = int;
+global using GenericContainerParameterIndex = int;
+global using AssemblyTypeIndex = int;
+global using AssemblyExportedTypeIndex = int;
+
+global using EncodedMethodIndex = uint;
+
+global using FullSizeTypeDefinitionIndex = int;
+
+namespace Il2CppInspector.Next.Indexing;
+
+public struct Type;
+public struct GenericParameter;
+public struct GenericContainer;
+public struct TypeDefinition;
+public struct Parameter;
+public struct Interfaces;
+public struct Event;
+public struct Property;
+public struct NestedType;
+public struct Field;
+public struct DefaultValueData;
+public struct Method;
+public struct InvokerTable;
+public struct MethodPointerTable;
+public struct AdjustorThunk;
+public struct GenericInst;
+public struct GenericMethod;
